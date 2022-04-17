094c9d1b4c68771c2820bc5874543af292bd7750
aredra <rnswn48@gmail.com>
2022-04-17 19:03:42 +0900

    Setting basic library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </>
 }
 
 export default MyApp
